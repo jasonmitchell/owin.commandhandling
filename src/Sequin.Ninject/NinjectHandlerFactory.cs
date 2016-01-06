40a1ef6bc1353076ce4395e4a5d1210159c165b9
Phillip Marsden <philyorkshire@philyorkshire.uk>
2016-01-06 22:37:09 +0000

    Added ninject factory handler

---
+++ src/Sequin.Ninject/NinjectHandlerFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Sequin.Ninject
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Core;
+    using Core.Infrastructure;
+
+    using global::Ninject;
+
+    public class NinjectHandlerFactory : IHandlerFactory
+    {
+        private readonly IKernel kernel;
+
+        public NinjectHandlerFactory(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public ICollection<IHandler<T>> GetForCommand<T>()
+        {
+            return kernel.GetAll<T>().OfType<IHandler<T>>().ToList();
+        }
+    }
+}
